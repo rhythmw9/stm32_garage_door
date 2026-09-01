e8f6ac80f5fb74f554745abc5fb2590d9c2433b2
Rhythm <rwinicourfreeman@gmail.com>
2026-09-01 13:13:21 -0700

    Updated rcc files

--- inc/rcc.h
+++ inc/rcc.h
@@ -10,6 +10,7 @@
 #define RCC_AHB1ENR *(volatile uint32_t*)(RCC_BASE + RCC_AHB1ENR_OFFSET)
 #define RCC_APB1ENR *(volatile uint32_t*)(RCC_BASE + RCC_APB1ENR_OFFSET)
 
+/* Function Prototypes */
 void periphClockEnable(void);
 
 #endif /* RCC_H */
--- src/rcc.c
+++ src/rcc.c
@@ -1,10 +1,14 @@
 #include "../inc/rcc.h"
 
+/* Bit Fields */
+#define GPIOA_CLK_EN (1U << 0)
+#define GPIOC_CLK_EN (1U << 2)
+
 void periphClockEnable(void)
 {
     // GPIOA
-    RCC_AHB1ENR |= (1U << 0);
+    RCC_AHB1ENR |= GPIOA_CLK_EN;
 
     // GPIOC
-    RCC_AHB1ENR |= (1U << 2);
+    RCC_AHB1ENR |= GPIOC_CLK_EN;
 }
